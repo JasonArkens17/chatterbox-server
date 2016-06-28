7c0602f3b3804d77a2597cf7cd9008502da7091b
JasonArkens17 <jason.arkens17@gmail.com>
2016-06-27 20:45:23 -0700

    Passed all the test cases for the bare minimum requirement.

--- server/request-handler.js
+++ server/request-handler.js
@@ -11,8 +11,51 @@ this file and include it in basic-server.js so that it actually works.
 *Hint* Check out the node module documentation at http://nodejs.org/api/modules.html.
 
 **************************************************************/
+// var headers = defaultCorsHeaders;
+// headers['Content-Type'] = 'text/plain';
+// var statusCode = 200;
+// response.writeHead(statusCode, headers);
+
+var messages = {results: []}; 
 
 var requestHandler = function(request, response) {
+
+  if (request.url !== '/classes/messages') {
+    var status = 404; 
+    var headers = defaultCorsHeaders; 
+    headers['Content-Type'] = 'text/plain'; 
+    response.writeHead(status, headers); 
+    response.end('Page not found!'); 
+
+  } else if (request.method === 'POST') {
+    var status = 201; 
+    var headers = defaultCorsHeaders; 
+    headers['Content-Type'] = 'text/plain'; 
+    response.writeHead(status, headers); 
+    var body = ''; 
+    request.on('data', function(chunk) {
+      body += chunk; 
+    });
+
+    request.on('end', function() {
+      // messages = Buffer.concat(messages).toString(); 
+      messages.results.push(JSON.parse(body)); 
+    }); 
+    response.end('DONE!'); 
+
+  } else if (request.method === 'GET') {
+    var status = 200; 
+    var headers = defaultCorsHeaders; 
+    headers['Content-Type'] = 'text/plain'; 
+    response.writeHead(status, headers); 
+
+    response.end(JSON.stringify({
+      headers: headers, 
+      status: status, 
+      method: 'GET',
+      results: messages.results
+    }));
+  }
   // Request and Response come from node's http module.
   //
   // They include information about both the incoming request, such as
@@ -29,30 +72,38 @@ var requestHandler = function(request, response) {
   // console.logs in your code.
   console.log('Serving request type ' + request.method + ' for url ' + request.url);
 
-  // The outgoing status.
-  var statusCode = 200;
+  // // The outgoing status.
+ 
 
-  // See the note below about CORS headers.
-  var headers = defaultCorsHeaders;
+  // // See the note below about CORS headers.
+ 
 
-  // Tell the client we are sending them plain text.
-  //
-  // You will need to change this if you are sending something
-  // other than plain text, like JSON or HTML.
-  headers['Content-Type'] = 'text/plain';
+  // // Tell the client we are sending them plain text.
+  // //
+  // // You will need to change this if you are sending something
+  // // other than plain text, like JSON or HTML.
+ 
 
-  // .writeHead() writes to the request line and headers of the response,
-  // which includes the status and all headers.
-  response.writeHead(statusCode, headers);
+  // // .writeHead() writes to the request line and headers of the response,
+  // // which includes the status and all headers.
+  
 
-  // Make sure to always call response.end() - Node may not send
-  // anything back to the client until you do. The string you pass to
-  // response.end() will be the body of the response - i.e. what shows
-  // up in the browser.
-  //
-  // Calling .end "flushes" the response's internal buffer, forcing
-  // node to actually send all the data over to the client.
-  response.end('Hello, World!');
+  // // Make sure to always call response.end() - Node may not send
+  // // anything back to the client until you do. The string you pass to
+  // // response.end() will be the body of the response - i.e. what shows
+  // // up in the browser.
+  // //
+  // // Calling .end "flushes" the response's internal buffer, forcing
+  // // node to actually send all the data over to the client.
+  // response.end('Hello, World!');
+
+
+
+
+
+
+
+
 };
 
 // These headers will allow Cross-Origin Resource Sharing (CORS).
@@ -71,4 +122,4 @@ var defaultCorsHeaders = {
   'access-control-max-age': 10 // Seconds.
 };
 
-exports.requestHandler = requestHandler;
+module.exports.requestHandler = requestHandler;
